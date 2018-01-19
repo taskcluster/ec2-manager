c5333bcd2a5dfbb5be31c056b7a0a8c408496053
John Ford <john@johnford.org>
2018-01-19 16:21:31 +0100

    We shouldn't delete instances from the db in the api
    This is because the cloudwatch-event handler is handling the
    shutting-down and terminated messages.

--- test/state_test.js
+++ test/state_test.js
@@ -438,7 +438,6 @@ describe('State', () => {
       id: 'i-0',
     });
 
-    console.dir(actual);
     assume(actual[0]).has.property('code', 'Code');
     assume(actual[0]).has.property('reason', 'Reason');
   });
