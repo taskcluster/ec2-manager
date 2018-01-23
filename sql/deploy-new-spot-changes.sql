3d834d1f9453d53c16250af88e279d0db08175cb
John Ford <john@johnford.org>
2018-01-23 16:58:20 +0100

    Add terminations table in deploy script

--- sql/deploy-new-spot-changes.sql
+++ sql/deploy-new-spot-changes.sql
@@ -16,3 +16,28 @@ ALTER TABLE instances RENAME COLUMN lastevent to "lastEvent";
 
 -- Fix amiusage table cases
 ALTER TABLE amiusage RENAME COLUMN lastused to "lastUsed";
+
+-- termination reasons
+CREATE TABLE IF NOT EXISTS terminations (
+  id VARCHAR(128) NOT NULL, -- opaque ID per Amazon
+  "workerType" VARCHAR(128) NOT NULL, -- taskcluster worker type
+  region VARCHAR(128) NOT NULL, -- ec2 region
+  az VARCHAR(128) NOT NULL, -- availability zone
+  "instanceType" VARCHAR(128) NOT NULL, -- ec2 instance type
+  "imageId" VARCHAR(128) NOT NULL, -- AMI/ImageId value
+  code VARCHAR(128), -- the State Reason's code
+  reason VARCHAR(128), -- the State Reason's string message
+  launched TIMESTAMPTZ NOT NULL, -- Time instance launched
+  terminated TIMESTAMPTZ, -- Time the instance shut down
+  "lastEvent" TIMESTAMPTZ NOT NULL, -- Time that the last event happened in the api. Used
+                                    -- to ensure that we have correct ordering of cloud watch
+                                    -- events
+  touched TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  PRIMARY KEY(id, region)
+);
+-- Automatically keep instances touched parameter up to date
+CREATE TRIGGER update_terminations_touched
+BEFORE UPDATE ON terminations
+FOR EACH ROW EXECUTE PROCEDURE update_touched();
+
+
